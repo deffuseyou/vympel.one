4eb2cc8bb5a4632f310f12d9df7ee7ada0f146b7
deffuseyou <kriulindmitry@mail.ru>
2023-06-11 15:32:09 +0300

    path_monitor fix

---
+++ path_monitor.py
@@ -0,0 +1,38 @@
+import time
+import telegram
+from watchdog.events import *
+from watchdog.observers import Observer
+from data_processing import config_read
+
+token = os.environ['vm_bot_token']
+bot = telegram.Bot(token=token)
+
+
+class PathHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        path = event.src_path.replace(config_read()["monitor-path"] + "\\", "")
+        if 'TeraCopy' not in path:
+            for telegram_id in config_read()['admin-telegram-id']:
+                bot.send_message(telegram_id, f'<b>{path}</b>', parse_mode='html')
+
+    # def on_modified(self, event):
+    #     path = event.src_path.replace(config_read()["monitor-path"] + "\\", "")
+    #     if 'TeraCopy' not in path:
+    #         for telegram_id in config_read()['admin-telegram-id']:
+    #             bot.send_message(telegram_id, f'изменено:\n<b>{path}\n</b>', parse_mode='html')
+
+
+if __name__ == "__main__":
+    path = config_read()["monitor-path"]
+    path_handler = PathHandler()
+    observer = Observer()
+    observer.schedule(path_handler, path, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(60)
+
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
